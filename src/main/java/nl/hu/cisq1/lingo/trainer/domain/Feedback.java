54c02bbf14962d4403a3912c412d12fd954181e6
Michel <m.kwantes@hotmail.com>
2021-04-02 13:27:00 +0200

    Deelopdracht 3, added exception throws in Feedback Constructor

--- src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/trainer/domain/Feedback.java
@@ -2,16 +2,53 @@ package nl.hu.cisq1.lingo.trainer.domain;
 
 
 
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidGuessLengthException;
+
 import java.util.List;
+import java.util.Objects;
 
 public class Feedback {
     private String attempt;
     private List<Mark> marks;
 
     public Feedback(String attempt, List<Mark> marks) {
-        this.attempt = attempt;
-        this.marks = marks;
+        if (attempt.length() == marks.size()) {
+            this.attempt = attempt;
+        } else {
+            throw new InvalidGuessLengthException();
+        }
+
+        if (isGuessValid(marks)) {
+            this.marks = marks;
+        } else {
+            throw new InvalidFeedbackException();
+        }
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Feedback feedback = (Feedback) o;
+        return Objects.equals(attempt, feedback.attempt) && Objects.equals(marks, feedback.marks);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attempt, marks);
+    }
+
+    private boolean isGuessValid(List<Mark> marks) {
+        for (Mark mark : marks) {
+            if (mark == Mark.INVALID) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
     public boolean isWordGuessed() {
         for (int i = 0; i < this.attempt.length(); i++) {
@@ -20,5 +57,13 @@ public class Feedback {
             }
         }
         return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "attempt='" + attempt + '\'' +
+                ", marks=" + marks +
+                '}';
     }
 }
--- src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/trainer/domain/FeedbackTest.java
@@ -1,5 +1,7 @@
 package nl.hu.cisq1.lingo.trainer.domain;
 
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidFeedbackException;
+import nl.hu.cisq1.lingo.trainer.domain.exception.InvalidGuessLengthException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,8 +21,20 @@ class FeedbackTest {
     @Test
     @DisplayName("Word is not guessed if some letters are not correct")
     void wordIsNotGuessed() {
-        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.INVALID));
+        Feedback feedback = new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.PRESENT));
         assertFalse(feedback.isWordGuessed());
+    }
+
+    @Test
+    @DisplayName("Throws exception when the marks dont add up to the length of the word")
+    void exceptionIsThrown() {
+        assertThrows(InvalidGuessLengthException.class, () -> new Feedback("woord", List.of(Mark.CORRECT) ));
+    }
+
+    @Test
+    @DisplayName("Throws InvalidFeedbackException")
+    void invalidFeedbackExceptionThrown() {
+        assertThrows(InvalidFeedbackException.class, () -> new Feedback("woord", List.of(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.INVALID)));
     }
 
 }
